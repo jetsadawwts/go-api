8bdac973f8ae60cb1529af940830dc30c4a43c93
jetsadawwts <jetsadawwts@gmail.com>
2022-12-26 10:42:06 +0700

    Ep7 go graceful shutdown

---
+++ graceful-shutdown/goroutine.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func slow(s string) {
+	for i := 0; i < 3; i++ {
+		time.Sleep(1 * time.Second)
+		fmt.Println(s, ":", i)
+	}
+}
+
+func main() {
+	done := make(chan bool)
+
+	go func ()  {
+		slow("gopher")
+		done <- true
+	}()
+	
+	slow("aun")
+
+	<-done
+	fmt.Println("all task done.")
+
+
+	// go slow("gopher")
+	// slow("aun")
+}
